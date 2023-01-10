eb23b4bb3b6f9362a5a9f7a8f19131520e70dd45
cokejnr <91469666+Cokejnr7@users.noreply.github.com>
2023-01-10 21:28:59 +0000

    added exit feauture

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from calculator import Calculator
 from composer import compose
 
+start = True
 if __name__ == '__main__':
-    exp = input('write an expression: ')
-    exp = compose(exp)
-    try:
-        calc = Calculator(exp)
-        print(calc.calculate())
-    except Exception as e:
-        print(f' Error {e} '.center(50, '-'))
+    while start:
+        exp = input('write an expression: ')
+        if exp.strip() == "exit":
+            start = False
+            print("terminated sucessfully")
+            break
+        exp = compose(exp)
+        try:
+            calc = Calculator(exp)
+            print(calc.calculate())
+        except Exception as e:
+            print(f' Error {e} '.center(50, '-'))
+        finally:
+            print('write exit and press enter to terminate')
